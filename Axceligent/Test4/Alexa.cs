2ded58dae319fcd81783f28f6a3bf4bed80c08ba
Ramin <ramin.mh2000@gmail.com>
2018-11-19 15:21:16 -0800

    Question Number 4

---
+++ Axceligent/Test4/Alexa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Test4
+{
+    internal class Alexa
+    {
+        public AlexaConfigurations _configurations;
+        public void Configure(Action<AlexaConfigurations> action)
+        {
+            action(_configurations ?? (_configurations=new AlexaConfigurations())) ;
+        }
+
+        public string Talk()
+        {
+            return _configurations==null ? "hello, i am Alexa" :
+                _configurations.GreetingMessage.Replace("{OwnerName}", _configurations.OwnerName);
+        }
+    }
+}
---
+++ Axceligent/Test4/AlexaConfigurations.cs
@@ -0,0 +1,8 @@
+﻿namespace Test4
+{
+    public class AlexaConfigurations
+    {
+        public string GreetingMessage { get; set; }
+        public string OwnerName { get; set; }
+    }
+}
