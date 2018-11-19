7f7c4ca0f2c3e4903c2207610ea495ab83adca8a
Ramin <ramin.mh2000@gmail.com>
2018-11-19 15:30:21 -0800

    Question Number  5

---
+++ Axceligent/Test5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CallMethod();
+        }
+
+        private static async void CallMethod()
+        {
+            var (averageSalary, numberOfEmployee) = await SomeCalculation(0L, 10L, 0L == 10L);
+        }
+
+        private static async Task<Tuple<int, int>> SomeCalculation(long l, long l1, bool b)
+        {
+          return new Tuple<int, int>(0,0);
+        }
+    }
+}
